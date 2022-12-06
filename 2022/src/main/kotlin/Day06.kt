b270ca183f6ee5498c5633f0228d1f1e8a042662
Kash Kabeya <nlkabeya@googlemail.com>
2022-12-06 18:21:38 +0000

    Day 6 of 2022

---
+++ 2022/src/main/kotlin/Day06.kt
@@ -0,0 +1,25 @@
+
+private const val PART_ONE_CHUNK_SIZE = 4
+private const val PART_TWO_CHUNK_SIZE = 14
+private val expectedStartsForPartOne = listOf(7, 5, 6, 10, 11)
+private val expectedStartsForPartTwo = listOf(19, 23, 23, 29, 26)
+
+fun main() {
+    fun part1(input: String): Int = findStart(input, PART_ONE_CHUNK_SIZE)
+    fun part2(input: String): Int = findStart(input, PART_TWO_CHUNK_SIZE)
+
+    val input = readText("Day06")
+    val inputTest = readLines("Day06-Test")
+
+    inputTest.forEachIndexed { idx, string -> check(part1(string) == expectedStartsForPartOne[idx]) }
+    println(part1(input))
+
+    inputTest.forEachIndexed { idx, string -> check(part2(string) == expectedStartsForPartTwo[idx]) }
+    println(part2(input))
+}
+
+private fun findStart(input: String, chunks: Int): Int {
+    for (i in (chunks..input.lastIndex))
+        if (input.substring(i - chunks, i).toSet().size == chunks) return i
+    return -1
+}
