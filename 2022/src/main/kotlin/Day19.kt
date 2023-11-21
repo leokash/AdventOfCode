8a8255be917d60652428009edee112ce57f29dc7
Kash Kabeya <nlkabeya@googlemail.com>
2023-11-21 10:08:06 +0000

    Modified Day 19 of 2022 to use less memory by make use of `IntArray`s to track robots and resources. Using a `Long` to represent the state of visited robots for a simulated day.

--- 2022/src/main/kotlin/Day19.kt
+++ 2022/src/main/kotlin/Day19.kt
@@ -3,159 +3,110 @@ import com.github.leokash.adventofcode.utils.*
 import com.github.leokash.adventofcode.utils.collections.*
 import kotlin.math.max
 
+private const val ORE = 0
+private const val CLAY = 1
+private const val GEODE = 3
+private const val OBSIDIAN = 2
 private const val PART_ONE_EXPECTED = 33
 private const val PART_TWO_EXPECTED = 3472
 
-private sealed class Material {
-    val index: Int get() = when (this) {
-        is Ore -> 0
-        is Clay -> 1
-        is Geode -> 3
-        is Obsidian -> 2
-    }
-    abstract fun timeThreshold(quality: Boolean): Int
+private typealias Pricing = Pair<Int, Int>
+private val Pricing.cost: Int get() = second
+private val IntArray.string: String get() = "${this[0]}${this[1]}${this[2]}${this[3]}"
 
-    override fun toString(): String {
-        return "${this::class.simpleName}"
+private fun IntArray.copy(): IntArray = IntArray(this.size) { i -> this@copy[i] }
+
+private fun IntArray.inc(idx: Int) { this[idx] += 1 }
+private fun IntArray.collect(robots: IntArray) { robots.forEachIndexed { i, m -> this@collect[i] += m } }
+private fun IntArray.buy(pricing: List<Pricing>) { pricing.forEach { (min, cost) -> this[min] -= cost } }
+
+private fun Int.cutOffTime(quality: Boolean): Int = when(this) {
+    ORE -> if (quality) 17 else 23
+    CLAY -> if (quality) 7 else 11
+    OBSIDIAN -> if (quality) 4 else 7
+    else -> 2
+}
+
+private fun IntArray.canFulfill(pricing: List<Pricing>): Boolean {
+    return pricing.all { (min, cost) -> this[min] >= cost }
+}
+
+private fun Map<Int, List<Pricing>>.possibleBots(time: Int, quality: Boolean, robots: IntArray, resources: IntArray, maxRobotsCache: Map<Int, Int>): List<Pair<Int, List<Pricing>>> {
+    fun canBuildRobotForMineral(m: Int): Boolean {
+        return time >= m.cutOffTime(quality) && robots[m] < maxRobotsCache.getValue(m)
     }
-    companion object {
-        fun from(s: String): Material {
-            return when (s) {
-                "ore" -> Ore
-                "clay" -> Clay
-                "geode" -> Geode
-                "obsidian" -> Obsidian
-                else -> error("invalid material provided: $s")
+
+    return buildList {
+        for (mineral in 3 downTo 0) {
+            val price = this@possibleBots.getValue(mineral)
+            if (canBuildRobotForMineral(mineral) && resources.canFulfill(price)) {
+                add(mineral to price)
+                if (mineral == GEODE) break
             }
         }
-    }
-}
-
-private object Ore: Material() {
-    override fun timeThreshold(quality: Boolean): Int = if (quality) 17 else 23
-}
-private object Clay: Material() {
-    override fun timeThreshold(quality: Boolean): Int = if (quality) 7 else 11
-}
-private object Geode: Material() {
-    override fun timeThreshold(quality: Boolean): Int = 2
-}
-private object Obsidian: Material() {
-    override fun timeThreshold(quality: Boolean): Int = if (quality) 4 else 5
-}
-
-private data class Blueprint(val id: Int, val requirements: Map<Material, List<Int>>) {
-    private val oreBots = listOf(Clay, Obsidian, Geode)
-    private val priority = mutableListOf(Geode, Obsidian, Clay, Ore)
-
-    val maxOreNeeded = requirements.maxOf { (m, items) -> if (m in oreBots) items[0] else 0 }
-    val maxClayNeeded = requirements.maxOf { (m, items) -> if (m is Obsidian) items[1] else 0 }
-    val maxObsidianNeeded = requirements.maxOf { (m, items) -> if (m is Geode) items[2] else 0 }
-
-    fun robotsAvailable(resources: Resources, robots: Robots, time: Int, quality: Boolean): List<Pair<Int, List<Int>>> {
-        fun canBuild(robot: Material): Boolean {
-            val threshold = robot.timeThreshold(quality)
-            return when (robot) {
-                is Ore -> time >= threshold && robots.ore < maxOreNeeded
-                is Clay -> time >= threshold && robots.clay < maxClayNeeded
-                is Obsidian -> time >= threshold && robots.obsidian < maxObsidianNeeded
-                else -> time >= threshold
-            }
-        }
-        fun Resources.canFulfill(req: List<Int>): Boolean {
-            return ore >= req[0] && clay >= req[1] && obsidian >= req[2]
-        }
-
-        return buildList {
-            for (product in priority) {
-                val required = requirements[product] ?: continue
-                if (canBuild(product) && resources.canFulfill(required)) {
-                    add(product.index to required)
-                    if (product is Geode) break
-                }
-            }
-        }
-    }
-}
-private data class Robots(var ore: Int = 1, var clay: Int = 0, var obsidian: Int = 0, var geode: Int = 0) {
-    fun add(i: Int) {
-        when (i) {
-            0 -> ore += 1
-            1 -> clay += 1
-            2 -> obsidian += 1
-            else -> geode += 1
-        }
-    }
-    override fun toString(): String {
-        return "$ore$clay$obsidian$geode"
-    }
-}
-private data class Resources(var ore: Int = 0, var clay: Int = 0, var obsidian: Int = 0, var geode: Int = 0) {
-    fun buy(cost: List<Int>) {
-        ore -= cost[0]; clay -= cost[1]; obsidian -= cost[2]
-    }
-    fun collect(bot: Robots) {
-        ore += bot.ore; clay += bot.clay; obsidian += bot.obsidian; geode += bot.geode
-    }
-
-    override fun toString(): String {
-        return "$ore$clay$obsidian$geode"
     }
 }
 
 fun main() {
     Logger.debug = false
-    data class State(val time: Int, val robots: String, val resources: String)
-    fun simulate(time: Int, print: Blueprint, bots: Robots = Robots(), res: Resources = Resources(), quality: Boolean, visited: MutableSet<State>): Int {
-        var count = res.geode
-        val state = State(time, bots.toString(), res.toString())
+    fun compute(blueprints: Map<Int, Map<Int, List<Pricing>>>, partOne: Boolean): Int {
+        val maxRobotsCache: Map<Int, Map<Int, Int>> = blueprints.mapValues { (_, map) ->
+            buildMap {
+                map.forEach { (min, _)  ->
+                    when(min) {
+                        ORE -> put(ORE, map.entries.filter { it.key != ORE }.maxOf { it.value[0].cost })
+                        CLAY -> put(CLAY, map.getValue(OBSIDIAN)[1].cost)
+                        OBSIDIAN -> put(OBSIDIAN, map.getValue(GEODE)[1].cost)
 
-        if (time <= 0 || state in visited)
-            return count
-
-        visited.add(state)
-        for ((robot, cost) in print.robotsAvailable(res, bots, time, quality)) {
-            val nBot = bots.copy().apply { add(robot) }
-            val nRes = res.copy().apply { buy(cost); collect(bots) }
-            count = max(count, simulate(time - 1, print, nBot, nRes, quality, visited))
-        }
-
-        return max(count, simulate(time - 1, print, bots, res.apply { collect(bots) }, quality, visited))
-    }
-
-    fun part1(input: List<String>): Int {
-        return parse(input).sumOf { b ->
-            b.id * simulate(24, b, quality = true, visited = mutableSetOf())
-        }
-    }
-    fun part2(input: List<String>): Int {
-        return parse(input)
-            .take(3)
-            .product { b ->
-                simulate(32, b, quality = false, visited = mutableSetOf())
+                        else -> put(GEODE, Int.MAX_VALUE)
+                    }
+                }
             }
+        }
+
+        fun simulate(bpId: Int, time: Int, robots: IntArray, resources: IntArray, states: MutableSet<Long>): Int {
+            var count = resources[GEODE]
+            val cache = maxRobotsCache.getValue(bpId)
+            val state = "$time${robots.string}${resources.string}".toLong()
+            if (time <= 0 || state in states)
+                return count
+
+            states.add(state)
+            for ((robot, price) in blueprints.getValue(bpId).possibleBots(time, partOne, robots, resources, cache)) {
+                val nBots = robots.copy().apply { inc(robot) }
+                val nResources = resources.copy().apply { buy(price); collect(robots) }
+                count = max(count, simulate(bpId, time - 1, nBots, nResources, states))
+            }
+
+            return max(count, simulate(bpId, time - 1, robots, resources.apply { collect(robots) }, states))
+        }
+
+        return when(partOne) {
+            true  -> blueprints.map { (id, _) -> id * simulate(id, 24, intArrayOf(1, 0, 0, 0), IntArray(4), mutableSetOf()) }.sum()
+            false -> blueprints.keys.sorted().take(3).product { simulate(it, 32, intArrayOf(1, 0, 0, 0), IntArray(4), mutableSetOf()) }
+        }
     }
 
-    val input = readLines("Day19")
-    val sample = readLines("Day19-Test")
+    val input = readLines("Day19").let { parse(it) }
+    val sample = readLines("Day19-Test").let { parse(it) }
 
-    check(part1(sample) == PART_ONE_EXPECTED)
-    println(part1(input))
+    check(compute(sample, true) == PART_ONE_EXPECTED)
+    println(compute(input, true))
 
-    check(part2(sample) == PART_TWO_EXPECTED)
-    println(part2(input))
+    check(compute(sample, false) == PART_TWO_EXPECTED)
+    println(compute(input, false))
 }
 
-private val idRgx = Regex("""Blueprint (\d+)""")
-private val rulesRgx = Regex("""Each (\w+) robot costs (\d+) (\w+)( and (\d+) (\w+))?""")
+private val id_regex = Regex("""Blueprint (\d+)""")
+private val pricing_regex = Regex("""Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.""")
 
-private fun parse(input: List<String>): List<Blueprint> {
-    return input.map { string ->
-        val id = string.matchingGroups(idRgx).first().toInt()
-        val recipes = string
-            .substringAfter(":").trim().split(".").dropLast(1).map { it.matchingGroups(rulesRgx) }
-        Blueprint(id, recipes.associate { (r, c1, _, _, c2, m2) ->
-            Material.from(r) to listOf(c1.toInt(), if (m2 == "clay") c2.toInt() else 0, if (m2 == "obsidian") c2.toInt() else 0)
-        })
+private fun parse(input: List<String>): Map<Int, Map<Int, List<Pricing>>> {
+    return buildMap {
+        input.forEach { string ->
+            val id = string.matchingGroups(id_regex).first().toInt()
+            this[id] = string.matchingGroups(pricing_regex)
+                .map { it.toInt() }
+                .let { mutableMapOf(ORE to listOf(ORE to it[0]), CLAY to listOf(ORE to it[1]), OBSIDIAN to listOf(ORE to it[2], CLAY to it[3]), GEODE to listOf(ORE to it[4], OBSIDIAN to it[5])) }
+        }
     }
 }
