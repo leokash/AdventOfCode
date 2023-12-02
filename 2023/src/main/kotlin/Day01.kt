3b3b30dbd3e3a95a45b198c695f1186e7aa5ef2d
Kash Kabeya <nlkabeya@googlemail.com>
2023-12-02 18:01:32 +0000

    Clean-up of AOC 2023: Day 1

--- 2023/src/main/kotlin/Day01.kt
+++ 2023/src/main/kotlin/Day01.kt
@@ -8,24 +8,25 @@ private val map: Map<String, Int> = mapOf(
     "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9
 )
 
+private val regex = """(\d|one|t(wo|hree)|f(our|ive)|s(ix|even)|eight|nine)""".toRegex()
+
+private fun String.toDigit(allowStringDigit: Boolean): Int? = when(this.length) {
+    1 -> first().digitToInt()
+    else -> if (allowStringDigit) map[this] else null
+}
+
 fun main() {
-    fun part1(input: List<String>): Int = input.sumOf { string ->
+    fun compute(input: List<String>, allowStringDigit: Boolean = false): Int = input.sumOf { string ->
         string
-            .mapNotNull { it.digitToIntOrNull() }
-            .let { (it.first() * 10) + it.last() }
+            .findAll(regex)
+            .mapNotNull { it.toDigit(allowStringDigit) }
+            .let { it.first() * 10 + it.last() }
     }
 
-    fun part2(input: List<String>): Int = input.sumOf { string ->
-        string.mapIndexedNotNull { i, c ->
-            c.digitToIntOrNull() ?: map.keys
-                .filter { string[i] == it.first() }
-                .firstOrNull { string[i..(i + it.length)] == it }
-                ?.let { map.getValue(it) }
-        }.let { (it.first() * 10) + it.last() }
-    }
+    fun part1(input: List<String>) = compute(input)
+    fun part2(input: List<String>) = compute(input, true)
 
     val input = readLines("Day01")
-    
     check(part1(readLines("Day01-Test")) == PART_ONE_EXPECTED)
     check(part2(readLines("Day01-Test-2")) == PART_TWO_EXPECTED)
 
